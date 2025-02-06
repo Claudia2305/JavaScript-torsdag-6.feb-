ce5e0c60142f548544518746729722410890766e
Claudia2305 <claudia2305c@hotmail.com>
2025-02-06 10:55:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript-torsdag-6.feb-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Claudia2305/JavaScript-torsdag-6.feb-)
---
+++ script.js
@@ -0,0 +1,54 @@
+/*
+Datatyper
+
+Oppgaver
+1. Lag variabel name, age, isLearningJs, putt inn forskjellige datatyper om deg selv
+2. Console.log hver variabel
+3. Bruk console log for å finne datatypen til en variabel
+4. Lag en array av forskjellige ingredienser til en bakeoppskrift
+
+Funksjon
+
+Oppgaver
+
+1. lag en funksjon som console.log 'Hello World'
+2. Lag en funksjon som legger sammen 2 tall
+3. Lag en funksjon med en variabel som har en array og console.log arrayet
+
+
+*/
+
+//DATATYPER//
+
+//Number
+
+let number = 23;
+
+//String
+
+let name = 'Anders';
+
+//Boleoon/
+
+let isTeaching = true;
+
+let firstName;
+
+//Errordata
+
+/**
+ 
+UndeFined
+Null
+Nan
+ */
+
+// Array
+
+let shoppingList = ['såpe', 'Mel', 'Fjorland'];
+
+let numberList = [0, 3, 5];
+
+//Object
+
+let firstObject = {};
